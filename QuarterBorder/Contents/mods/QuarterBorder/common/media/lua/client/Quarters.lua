cc158fd96b871ee2c2718b488f4632fad3205396
molecule31 <73964434+molecule31@users.noreply.github.com>
2025-01-10 09:42:08 +0200

    Update Quarters.lua

--- QuarterBorder/Contents/mods/QuarterBorder/common/media/lua/client/Quarters.lua
+++ QuarterBorder/Contents/mods/QuarterBorder/common/media/lua/client/Quarters.lua
@@ -1,6 +1,7 @@
-if ISMoodlesInLua == nil then return end -- do not remove this
+if ISMoodlesInLuaHandle == nil then return end -- do not remove this
 
+-- Register texture set
 local name = "Quarters" -- Name of your texture set
 local path = "media/ui/MIL/Quarter" -- Path to your textures
 
-return ISMoodlesInLuaHandle:registerBorderTextureSet(name, path)
+ISMoodlesInLuaHandle:registerBorderTextureSet(name, path)
